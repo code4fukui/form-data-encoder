9d824bba69e1af9c0743bc468b7c2d772d5f8487
Taisuke Fukuno <fukuno@jig.jp>
2023-03-24 09:21:06 +0900

    fix doc

--- example.js
+++ example.js
@@ -1,10 +1,8 @@
 import { FormDataEncoder } from "./FormDataEncoder.js";
 
 const form = new FormData();
-form.set("a", 2);
-form.set("b", 20);
-//form.set("file", file, "a.txt");
-//form.set("file", new Blob(["abc"]), "a.txt");
+form.set("a", 15);
+form.set("b", "test");
 form.set("file", new Blob([new Uint8Array([1, 2, 3])]), "a.txt");
 
 const { headers, body } = await FormDataEncoder.encode(form);
